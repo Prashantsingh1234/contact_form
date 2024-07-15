d12fadcdb7a380f932a94032bfcf3f83f8ad3559
Prashantsingh1234 <your.email@example.com>
2024-07-15 11:29:58 +0530

    contact form using reactjs

---
+++ src/App.jsx
@@ -0,0 +1,7 @@
+import { ContactForm } from "./components/hooks/useState/ContactFrom";
+
+//subscribe to Thapa Technical Youtube Channel - https://www.youtube.com/thapatechnical
+
+export const App = () => {
+  return <ContactForm />;
+};
